d67afd090ea84b2706d240b5343f9985ce80713e
Gabriela <gabi.ivanova@gmail.com>
2016-05-28 12:42:27 +0300

    cat decoding

---
+++ C# Advanced/Fundamentals Exams/CatCoding.cs
@@ -0,0 +1,106 @@
+﻿namespace _01.DeCatCoding
+{
+    using System;
+    using System.Text;
+
+    class CatCoding
+    {
+        static void Main(string[] args)
+        {
+            string[] catMessage = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder decryptedMessage = new StringBuilder();
+
+            for (int i = 0; i < catMessage.Length; i++)
+            {
+                string currentWord = catMessage[i];
+                decimal number21System = 0;
+                decimal power = currentWord.Length - 1;
+
+                for (int k = 0; k < currentWord.Length; k++)
+                {
+                    char letter = currentWord[k];
+
+                    decimal letterNumber = FindNumberFromLetter(letter);
+
+                    decimal powerNumber = FindPowerNumber(power);
+
+                    number21System += letterNumber * powerNumber;
+
+                    power--;
+                }
+                //decimal number26System = ConvertToTwentySixSystem(number21System);
+
+                decryptedMessage.Append(ConvertToTwentySixSystem(number21System) + " ");
+            }
+
+            Console.WriteLine(decryptedMessage.ToString().Trim());
+        }
+
+        private static string ConvertToTwentySixSystem(decimal number21System)
+        {
+            StringBuilder decryptedWord = new StringBuilder();
+            long num = Convert.ToInt64(number21System);
+            while (num > 0)
+            {
+                long leftOver = num % 26;
+
+                decryptedWord.Append(FindLetterFromNumber(leftOver));
+
+                num /= 26;
+            }
+
+            char[] arr = decryptedWord.ToString().ToCharArray();
+            Array.Reverse(arr);
+
+            return new string(arr);
+            
+        }
+
+        private static char FindLetterFromNumber(long leftOver)
+        {
+            return Convert.ToChar(leftOver + 97);
+        }
+
+        private static decimal FindPowerNumber(decimal power)
+        {
+            decimal result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= 21;
+            }
+            return result;
+        }
+
+        private static decimal FindNumberFromLetter(char letter)
+        {
+            switch (letter)
+            {
+                case 'a': return 0; 
+                case 'b': return 1;
+                case 'c': return 2;
+                case 'd': return 3;
+                case 'e': return 4;
+                case 'f': return 5;
+                case 'g': return 6;
+                case 'h': return 7;
+                case 'i': return 8;
+                case 'j': return 9;
+                case 'k': return 10;
+                case 'l': return 11;
+                case 'm': return 12;
+                case 'n': return 13;
+                case 'o': return 14;
+                case 'p': return 15;
+                case 'q': return 16;
+                case 'r': return 17;
+                case 's': return 18;
+                case 't': return 19;
+                case 'u': return 20;
+
+                default: throw new NotImplementedException();
+            }
+            
+        }
+    }
+}
---
+++ C# Advanced/Fundamentals Exams/CatCoding.md
@@ -0,0 +1,22 @@
+Problem 1 – De-cat-coding
+At long last scientist found out that the cats have an evil plan to enslave all of humanity. The sinister organization Al cat-qaeda have been targeting high profile programmers and taking them down (a.k.a brutally murdering them), because they are the only ones that can ruin their plans.
+“But why programmers?” you would ask, because they are the only ones that can write a program to crack their communication codes. They are using a 21-based numeral system in order to fool everyone that they are stupid and harmless beings. The digits go as following:
+a	b	c	d	…	j	k	…	u
+0	1	2	3	…	9	10	…	20
+
+Your task is to help the global programmer community. You will receive a set on letter-numbers (strings) on one line separated with a single space ‘ ‘. You have to decode every word transferring it from 21-based numeral system to 26-based numeral system (the whole English alphabet).
+For example, if you have the word “cb” in decimal system that will be 2*21 + 1*1 = 43. That in 26-based system is 43 % 26 = 17 = ‘r’, 1 % 26 = 1 = ‘b’. So in 21-based “cb” is “br” in 26.based system.
+Input
+The input data should be read from the console.
+The input data consists of a single string that needs to be split.
+The input data will always be valid and in the format described. There is no need to check it explicitly.
+Output
+The output data should be printed on the console.
+Print out the decoded words separated by a single space.
+Constraints
+•	The number of letters-numbers will be between 3 and 20, inclusive.
+•	All of the given letter-numbers will consist of small characters.
+•	None of the letter-numbers will start with an ‘a’.
+•	The words in decimal system will always be between 0 and 18 446 744 073 709 551 615.
+•	Allowed working time for your program: 0.1 seconds.
+•	Allowed memory: 16 MB.
